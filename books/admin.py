faf7e3766ba57fc2cf8e76af994ac3dba2c6228f
Vodem <vadkirichuk@gmail.com>
2021-09-14 17:30:22 +0300

    исправить book/добавить остальным

--- books/admin.py
+++ books/admin.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Book, Seller, Order, Customer
 
 
 @admin.register(Customer)
 class AdminCustomer(admin.ModelAdmin):
-    pass
+    list_display = ['first_name', 'last_name']
 
 
 @admin.register(Seller)
 class AdminSeller(admin.ModelAdmin):
-    pass
+    list_display = ['first_name', 'last_name','position', 'recruitment']
 
 
 @admin.register(Book)
 class AdminBook(admin.ModelAdmin):
-    pass
+    list_display = ['name_product', 'price', 'genre', 'pic']
 
+    def pic(self, obj):
+        return mark_safe('<img src="{url}" width="150" height="200" />'.format(url=obj.img.url))
 
 @admin.register(Order)
 class AdminOrder(admin.ModelAdmin):
--- books/models.py
+++ books/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=64, blank=False)
-    last_name = models.CharField(max_length=64, blank=False)
-    phone = models.CharField(max_length=20, blank=False, unique=True)
-    email = models.EmailField(unique=True, blank=True)
-    discount = models.BooleanField(default=0, blank=False)
+    first_name = models.CharField(max_length=64, blank=False, verbose_name = 'Имя')
+    last_name = models.CharField(max_length=64, blank=False, verbose_name = 'Фамилия')
+    phone = models.CharField(max_length=20, blank=False, unique=True, verbose_name = 'Номер телефона')
+    email = models.EmailField(blank=True, verbose_name = 'Email')
+    discount = models.BooleanField(default=0, blank=False, verbose_name = 'Скидка')
 
     class Meta:
         verbose_name = 'Покупатель'
         verbose_name_plural = 'Покупатели'
 
 
-#     def __str__(self):
-#         return self.first_name + " " + self.last_name
-#
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Seller(models.Model):
     POSITION_LIST = [
@@ -24,12 +24,12 @@ class Seller(models.Model):
         ('Oldest salesman', "Старший продавец"),
         ('Supervisor', "Руководитель")
     ]
-    first_name = models.CharField(max_length=64, blank=False)
-    last_name = models.CharField(max_length=64, blank=False)
-    phone = models.CharField(max_length=20, blank=False, unique=True)
-    email = models.EmailField(unique=True, blank=True)
-    position = models.CharField(max_length=64, blank=False, default='Trainee', choices=POSITION_LIST)
-    img = models.ImageField(upload_to='seller_img/%Y/%m/%d', blank=True)
+    first_name = models.CharField(max_length=64, blank=False, verbose_name = 'Имя')
+    last_name = models.CharField(max_length=64, blank=False, verbose_name = 'Фамилия')
+    phone = models.CharField(max_length=20, blank=False, unique=True, verbose_name = 'Номер телефона')
+    email = models.EmailField(blank=True, verbose_name = 'Email')
+    position = models.CharField(max_length=64, blank=False, default='Trainee', choices=POSITION_LIST, verbose_name = 'Должность')
+    img = models.ImageField(upload_to='seller_img/%Y/%m/%d', blank=True, verbose_name = 'Изображение')
     recruitment = models.DateField(verbose_name="Принятие на работу")
 
     class Meta:
@@ -37,9 +37,9 @@ class Seller(models.Model):
         verbose_name_plural = "Продавцы"
 
 
-#     def __str__(self):
-#         return f'{self.first_name} {self.last_name} {self.position }'
-#
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.position }'
+
 
 class Book(models.Model):
     GENRE_LIST = [
@@ -49,19 +49,19 @@ class Book(models.Model):
         ('Psychology', 'Психолоия')
     ]
 
-    name_product = models.CharField(blank=False, max_length=64)
-    price = models.DecimalField(max_digits=12, decimal_places=2, blank=False)
-    genre = models.CharField(max_length= 64, blank=False, choices=GENRE_LIST, default='Detective')
-    img = models.ImageField(upload_to='books_img/%Y/%m/%d', blank=True, default='media/img.png')
+    name_product = models.CharField(blank=False, max_length=64, verbose_name = 'Название')
+    price = models.DecimalField(max_digits=12, decimal_places=2, blank=False, verbose_name = 'Цена')
+    genre = models.CharField(max_length= 64, blank=False, choices=GENRE_LIST, default='Detective', verbose_name = 'Жанр')
+    img = models.ImageField(upload_to='books_img/%Y/%m/%d', blank=True, default='media/img.png', verbose_name = 'Картинка')
 
     class Meta:
         verbose_name = 'Книга'
         verbose_name_plural = 'Книги'
 
 
-#     def __str__(self):
-#         return self.category + ' ' + self.name_product
-#
+    def __str__(self):
+        return self.genre + ' ' + self.name_product
+
 
 class Order(models.Model):
     customer = models.ForeignKey("Customer",
@@ -93,5 +93,5 @@ class Order(models.Model):
         verbose_name = 'Продажа'
         verbose_name_plural = 'Продажи'
 
-#     def __str__(self):
-#         return f'Продавец: {self.seller} Покупатель: {self.customer} {self.total}'
+    def __str__(self):
+        return f'Продавец: {self.seller} Покупатель: {self.customer} {self.total}'
