db409d05ddccbf51e88190a890c205af4134d83c
Vodem <vadkirichuk@gmail.com>
2021-09-15 17:43:48 +0300

    update в процессе

---
+++ books/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Book
+
+
+class UpdateBook(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        labels = {
+            'name_product': 'Название книги',
+            'price': "Цена",
+            "genre": "Жанр",
+            "img": "Изображение"
+        }
+
+        widgets = {
+            'name_product': forms.TextInput(attrs={'class':'form-control'}),
+            'price': forms.DecimalField(),
+            "genre": forms.ChoiceField(attrs={'class':'form-control', 'choice':Book.GENRE_LIST}),
+            "img": forms.ImageField(attrs={'class':'form-control'}),
+
+        }
--- books/views.py
+++ books/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.core.paginator import Paginator
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def book(request):
-    book = Book.objects.all()
+    if request.POST:
+        curr_element = request.POST.get('id', None)
+        elem = get_object_or_404(pk=curr_element, klass=Book)
+        elem.delete()
+        book = Book.objects.all()
+    else:
+        book = Book.objects.all()
     pagination = Paginator(book, 4)
     page = request.GET.get('page')
     page_obj = pagination.get_page(page)
-
     return render(request, 'book.html', {'book': page_obj})
+
+
+def update_book(request, book_id):
+    if request.GET:
+        
