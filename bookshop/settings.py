512cb0365e6d781fe28d346b73b687ce8e7d2e53
Vodem <vadkirichuk@gmail.com>
2021-09-20 20:43:09 +0300

    fix

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'books.apps.BooksConfig',
     'account.apps.AccountConfig',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
