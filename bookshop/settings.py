0713abd0f16dfe333f99af51ea680bbb660f395d
Vodem <vadkirichuk@gmail.com>
2021-09-20 20:26:30 +0300

    site

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -27,10 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
-import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Application definition
 
@@ -139,3 +136,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
